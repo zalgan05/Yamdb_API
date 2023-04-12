71d00f3790d347db3e92ee1b112d431805e90944
tryap <nom-de-guerre@mail.ru>
2023-04-12 10:17:34 +0300

    rebase `feature/19` to `dev`

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,6 +8,28 @@ from .permissions import IsAuthorOrModeratorOrAdminOrReadOnly
 from .serializers import ReviewSerializer
 
 
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .helpers_auth import send_signup_letter
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user, _ = User.objects.get_or_create(
+        username=serializer.data["username"],
+        email=serializer.data["email"],
+    )
+    send_signup_letter(user)
+    return Response()
+
+
 class ReviewViewSet(viewsets.ModelViewSet):
     """Обрабатывает запросы GET для всех отзывов, POST - создаёт новый отзыв,
     GET, PATCH, DELETE для одного отзыва по id отзыва."""
@@ -15,7 +37,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     pagination_class = LimitOffsetPagination
     permission_classes = (
-        IsAuthenticatedOrReadOnly, IsAuthorOrModeratorOrAdminOrReadOnly
+        IsAuthenticatedOrReadOnly,
+        IsAuthorOrModeratorOrAdminOrReadOnly,
     )
 
     def get_queryset(self):
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
         TemplateView.as_view(template_name="redoc.html"),
         name="redoc",
     ),
-    path('api/v1/', include('api.urls')),
+    path("api/", include("api.urls")),
 ]
