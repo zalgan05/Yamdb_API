e4a669b5f6e2c855f8f3e42dcb85a97588a2ed52
tryap <nom-de-guerre@mail.ru>
2023-04-17 12:54:56 +0300

    more viewset mixins

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,10 +1,25 @@
 from rest_framework import mixins, viewsets
 
 
-class ListCreateDestroyViewSet(
+class ListCreateViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
-    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
+    pass
+
+
+class RetrievUpdateViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    http_method_names = ['get', 'post', 'patch', 'delete']  # Убран метод PUT
+
+
+class DestroyViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class AllViewSet(ListCreateViewSet, RetrievUpdateViewSet, DestroyViewSet):
     pass
